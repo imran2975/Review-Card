1ae1f5d6ebbfd2ef13c79eef2ac8e6595d7e5304
Imran Abubakar <84298402+imran2975@users.noreply.github.com>
2022-11-24 01:19:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const reviews = [
   {
     id: 1,
-    name: "susan smith",
+    name: "imran abubakar",
     job: "web developer",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      "https://pbs.twimg.com/profile_images/1527789699657613317/_Es2l3FH_400x400.jpg",
     text:
       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
